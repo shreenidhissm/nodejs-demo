43b819bb32e5ac1b5dc368ea55c3d53e60f95460
avinashbasoor12 <34620046+avinashbasoor12@users.noreply.github.com>
2022-06-20 20:04:22 +0530

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello, Welcome to Avinash demo" }');
+    res.send('{ "response": "Hello, Welcome to Avinash demo app" }');
 });
 
 app.get('/will', function (req, res) {
