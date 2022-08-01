750e38ea8b36341fc0ecf66bf5939df8009f4d8b
Praveen <Praveen@gmail.com>
2022-08-01 01:17:08 +0000

    this is index.js file modification

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello, Welcome Basoor tech" }');
+    res.send('{ "response": "Hello, Welcome Demo project" }');
 });
 
 app.get('/will', function (req, res) {
